c665286b8763d17d395b096b1fa26434ffb0ce86
emanuel-araya <emanuel.araya@est.fi.uncoma.edu.ar>
2020-05-09 20:02:45 -0300

    vista rubros

---
+++ basic/views/rubros/create.php
@@ -0,0 +1,20 @@
+<?php
+
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Rubros */
+
+$this->title = 'Create Rubros';
+$this->params['breadcrumbs'][] = ['label' => 'Rubros', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="rubros-create">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
